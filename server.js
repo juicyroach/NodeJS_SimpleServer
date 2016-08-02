1e94bf77d659d3e4de3354ae93432331eee678af
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-08-02 11:26:26 -0700

    import server from main file
    use require to get the model we create

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-	response.writeHead(200, {
-		"Content-Type": "text/plain"
-	});
-	response.write("Hello world!");
-	response.end();
-}).listen(8888); // 8888 = port
+function start() {
+	function onRequest(request, response) {
+		console.log("Request receive");
+		response.writeHead(200, {
+			"Content-Type": "text/plain"
+		});
+		response.write("Hello world!");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server Start!");
+}
+
+exports.start = start;
