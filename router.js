553462f5c792dc8a0fb19ff5f0ffb24f4699fbc5
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-08-02 15:40:45 -0700

    Non-Blocking operation
    use response as object and make it to the non-blocking operation

--- router.js
+++ router.js
@@ -1,12 +1,13 @@
-function route(handle, pathName) {
-	console.log("About to route a request for " + pathName);
-	if (typeof handle[pathName] === 'function') {
-		return handle[pathName]();
-	} else {
-		console.log("No request handler found for " + pathName);
-		return "404 NOT FOUND!"
-	}
+function route(handle, pathName, response) {
+  console.log("About to route a request for " + pathName);
+  if (typeof handle[pathName] === 'function') {
+    handle[pathName](response);
+  } else {
+    console.log("No request handler found for " + pathName);
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not found");
+    response.end();
+  }
 }
-
 
 exports.route = route;
--- server.js
+++ server.js
@@ -2,20 +2,15 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-	function onRequest(request, response) {
-		var pathName = url.parse(request.url).pathname;
-		console.log("Request from " + pathName + " received");
+  function onRequest(request, response) {
+    var pathName = url.parse(request.url).pathname;
+    console.log("Request for " + pathName + " received.");
 
-		var content = route(handle, pathName);
+    route(handle, pathName, response);
+  }
 
-		response.writeHead(200, {
-			"Content-Type": "text/plain"
-		});
-		response.write(content);
-		response.end();
-	}
-	http.createServer(onRequest).listen(8888);
-	console.log("Server Start!");
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
 exports.start = start;
