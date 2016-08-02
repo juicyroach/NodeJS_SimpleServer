6a2bf50dba37d71ebe1265ebbd6cc39b0b6cd77c
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-08-02 12:17:49 -0700

    Separate router action

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathName = url.parse(request.url).pathname;
 		console.log("Request from " + pathName + " received");
 
-		route(pathName);
+		route(handle, pathName);
 
 		response.writeHead(200, {
 			"Content-Type": "text/plain"
