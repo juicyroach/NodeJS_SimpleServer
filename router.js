3f97b34935a58231b1067d511a7404827a3130c0
Tim_Yen <Pega@PegatekiMacBook-Pro.local>
2016-08-18 17:15:49 -0700

    add Data and end for post process

--- router.js
+++ router.js
@@ -1,13 +1,16 @@
-function route(handle, pathName, response) {
-  console.log("About to route a request for " + pathName);
-  if (typeof handle[pathName] === 'function') {
-    handle[pathName](response);
-  } else {
-    console.log("No request handler found for " + pathName);
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("404 Not found");
-    response.end();
-  }
+function route(handle, pathname, response, postData) {
+	console.log("About to route a request for " + pathname);
+	if (typeof handle[pathname] === 'function') {
+		handle[pathname](response, postData);
+	} else {
+		console.log("No request handler found for " + pathname);
+		response.writeHead(404, {
+			"Content-Type": "text/plain"
+		});
+		response.write("404 Not found");
+		response.end();
+	}
 }
+
 
 exports.route = route;
